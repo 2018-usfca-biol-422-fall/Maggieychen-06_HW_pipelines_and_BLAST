327b773001b1ba9acd28dbfa9d096dab71e4203e
Maggie Chen <ychen254@dons.usfca.edu>
2018-09-27 00:20:11 +0000

    Create and add descriptions to the script

---
+++ 2018-09-26_QC_BLAST.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Maggie Chen
+# 2018 September 26
+# ychen254@dons.usfca.edu
+
+# This is a script for download fastq file, run QC check, trimming and BLAST them
+# againist local copy of GenBank
+# The data being used is from the NCBI  sequence  Read Archive study number ERP022657
+# The data is from the research to demostrate that the skin-associated bacteria community composition  
