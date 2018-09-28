86ff6a335e5e8301213e7aaf478a548698e4f199
Maggie Chen <ychen254@dons.usfca.edu>
2018-09-28 23:25:35 +0000

    Add for loop for blast, modified for loop of trim and bioawk, script passed shellcheck

--- 2018-09-26_QC_BLAST.sh
+++ 2018-09-26_QC_BLAST.sh
@@ -13,39 +13,43 @@
 
 # Download the files in run table to the raw data directory
 # the pipe and tail -n +2 is a handy way to exclude the first line
-echo "Downloading files in run table to raw data directory....."
+echo "Downloading files in run table to raw_data directory....."
 for SRA_number in $(cut -f 6 data/metadata/fierer_forensic_hand_mouse_SraRunTable.txt | tail -n +2)
 do
-	fastq-dump -v $SRA_number -O data/raw_data
+	fastq-dump -v "$SRA_number" -O data/raw_data
 done
 
 # Create QC report for each of the  runs using FastQC program 
 # Can use 'cyberduck' to  tranfer reports to personal laptop
 # and view files in .html formate 
-echo "Generating QC report to output/fastqc directory"
+echo "Generating QC report to output/fastqc directory....."
 fastqc data/raw_data/*.fastq --outdir=output/fastqc
 
 # Trim the sequence base on their quality score
 # this will discard sequence under 150 base pairs
 # and cut off reads when he base score is lower than 25
-# create a new output directoryÃas data/trimmed
-echo "Trimming the sequences based on quality score"
-mkdir data/trimmed
-for filename "@$" 
-do 
-	TrimmomaticSE -threads 2 -phred33 data/raw_data/"$filename" data/trimmed/$(basename -s .fastq "$filename").trim.fastq LEADING:5 TRAILING:5 SLIDINGWINDOW:8:25 MINLEN:150
+# create a new output directory as data/trimmed
+# identify the location as data/raw_data/ when running the script
+echo "Trimming the sequences based on quality score into data/trimmed directory....."
+for filename in data/raw_data/*.fastq
+do
+        TrimmomaticSE -threads 2 -phred33 "$filename" data/trimmed/"$(basename -s .fastq "$filename")".trim.fastq LEADING:5 TRAILING:5 SLIDINGWINDOW:8:25 MINLEN:150
 done
 
-# Convert fastq files into fasta files
+# Convert fastq files into fasta files, save in data/trimmed directory
 # So files can be used as BLASR queries
-echo "Converting fastq to fasta files....."
-for filename "$@"
-do 
-	bioawk -c fastx '{print ">"$name"\n"$seq}' data/trimmed/"$filename"
+echo "Converting fastq to fasta files into data/trimmed directory....."
+for filename in data/trimmed/*.fastq
+do
+        bioawk -c fastx '{print ">"$name"\n"$seq}' "$filename" > data/trimmed/'$(basename -s .fastq "$filename").fasta'
 done
 
 
 # In the nt database, use blastn to search for the top matches of sequences
 # This will create a  blast_results.csv file as output in the output directory
-# and a query_seqs.fasta file, with one row for each uery sequence
-
+# and a query_seqs.fasta file, with one row for each query sequence
+echo "BLASTING fasta files, save results into output/blast directory....."
+for filename in data/trimmed/*.fasta
+do
+	blastn -db /blast-db/nt -num_threads 2 -outfmt '10 sscinames std' -out output/blast/"$(basename -s .trim.fasta "$filename")".csv -max_target_seqs 1 -negative_gilist /blast-db/2018-09-19_environmental_sequence.gi -query "$filename"
+done
